d58b950491ec486b4ce99754749cdb7cfbffb6a8
YumiClmpn <maecalumpiano@gmail.com>
2023-11-28 11:18:42 +0800

    added addiional problem solution samples

--- cn-io-problem.py
+++ cn-io-problem.py
@@ -1,12 +1,18 @@
 
-# solution sample 1
+# find area of a rectangle - solution sample 1
 length, breadth = input("Length and Breadth: ").split()
 area = int(length) * int(breadth)
 
 print("Area:", str(area))
 
-# solution sample 2
+# find area of a rectangle - solution sample 2
 length, breadth = input().split()
 area = int(length) * int(breadth)
 
 print(area)
+
+# print name and age problem  - solution
+name = input()
+age = input()
+
+print("The name of the person is", name, "and the age is", age + ".") # i need to use plus "+" sign to add period "." without space in printing
