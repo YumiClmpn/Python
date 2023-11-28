173e241195fef3cf8d5c7bd2fb0a12ef3b62a381
YumiClmpn <maecalumpiano@gmail.com>
2023-11-28 10:56:25 +0800

    adjusted names and added problem solutions from cn

---
+++ cn-io-problem.py
@@ -0,0 +1,12 @@
+
+# solution sample 1
+length, breadth = input("Length and Breadth: ").split()
+area = int(length) * int(breadth)
+
+print("Area:", str(area))
+
+# solution sample 2
+length, breadth = input().split()
+area = int(length) * int(breadth)
+
+print(area)
