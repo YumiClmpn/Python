fcdaec14d6fd9d4ee211f9e3dc68156f974d4908
YumiClmpn <maecalumpiano@gmail.com>
2023-12-02 16:15:39 +0800

    updated conditional problems notes :>

--- cn-conditionals-problem.py
+++ cn-conditionals-problem.py
@@ -42,5 +42,64 @@ def sumEvenOdd(number):
 N = int(input())
 evenSum, oddSum = sumEvenOdd(N)
 print(evenSum, oddSum)
-'''
-#  NOTE: please continue on the Find power of a number problem
+
+# Find power of a number
+x, n = map(int, input().split())
+
+if x == 0 and n == 0:
+    result = 1
+else:
+    result = str(x ** n)
+
+print(result)
+
+#  Factorial of a Number
+def calculate_factorial(n):
+    try:
+        n = int(n)
+        if n < 0:
+            return "Error"
+        factorial = 1
+        for i in range(1, n + 1):
+            factorial *= i
+        return factorial
+    except ValueError:
+        return "Error"
+
+n = input()
+result = calculate_factorial(n)
+print(result)
+
+#  N-th Fibonacci Number
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def multiply_matrix(A, B, mod):
+    result = [[0, 0], [0, 0]]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                result[i][j] = (result[i][j] + A[i][k] * B[k][j]) % mod
+    return result
+
+def power_matrix(A, n, mod):
+    if n == 1:
+        return A
+    if n % 2 == 0:
+        half_pow = power_matrix(A, n // 2, mod)
+        return multiply_matrix(half_pow, half_pow, mod)
+    else:
+        half_pow = power_matrix(A, (n - 1) // 2, mod)
+        return multiply_matrix(multiply_matrix(half_pow, half_pow, mod), A, mod)
+
+def fibonacciNumber(n):
+    if n <= 1:
+        return n
+    mod = 1000000007  # 1e9 + 7
+    base_matrix = [[1, 1], [1, 0]]
+    result_matrix = power_matrix(base_matrix, n - 1, mod)
+    return result_matrix[0][0]
+
+# NOTE: To study above
