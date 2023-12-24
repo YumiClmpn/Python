5b7f5019dd1907d8cd0ca41e8a96f23860817362
YumiClmpn <maecalumpiano@gmail.com>
2023-12-24 16:35:19 +0800

    added kape challenge entry

---
+++ kape-challenge.py
@@ -0,0 +1,10 @@
+# CHALLENGE 1
+print("=============================================================================")
+print("To get the area of a rectangle,  please input its length and its width below.")
+print("=============================================================================")
+
+length = input("Length: ")
+width = input("Width: ")
+area = int(length) * int(width)
+
+print("Area:", area)
